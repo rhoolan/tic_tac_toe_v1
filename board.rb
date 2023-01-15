f3806817ebf8e4e76dd15d6395a954bf4b11dd49
Rob Hoolan <rhoolan@gmail.com>
2023-01-16 00:41:49 +0900

    add valid? function

--- board.rb
+++ board.rb
@@ -5,4 +5,9 @@ class Board
         @grid = Array.new(3) { Array.new(3, '_')}
     end
 
+    #valid input will be [row, col]
+    def valid?(position)
+        position[0].between?(0,2) && position[1].between?(0,2)
+    end
+
 end

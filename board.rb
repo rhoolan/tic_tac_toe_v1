806e77c0576f38fd079f6898065ae438426e3ce6
Rob Hoolan <rhoolan@gmail.com>
2023-01-16 10:55:13 +0900

    add win_diag? function

--- board.rb
+++ board.rb
@@ -5,6 +5,11 @@ class Board
         @grid = Array.new(3) { Array.new(3, '_')}
     end
 
+    def [](pos)
+        row, col = pos
+        grid[row][col]
+    end
+
     #valid input will be [row, col]
     def valid?(position)
         position[0].between?(0,2) && position[1].between?(0,2)
@@ -20,7 +25,7 @@ class Board
         grid[position[0]][position[1]] = mark
     end
 
-    def print
+    def print_board
         puts grid.map { |row| row.join(' ')}
     end
 
@@ -30,6 +35,19 @@ class Board
 
     def win_col?(mark)
         return true if grid.transpose.each { |x| x.all? { |tile| tile == mark}}
+    end
+
+    def win_diag?(mark)
+        left_to_right = []
+        right_to_left = []
+
+        (0...grid.length).each do |i|
+            left_to_right << self[[i,i]]
+            right_to_left << self[[i,-(i+1)]]
+        end
+
+        return true if left_to_right.all?(mark) || right_to_left.all?(mark)
+
     end
 
 

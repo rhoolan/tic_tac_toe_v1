31996d1ffba5ee6c9c907cb6473093aed52caead
Rob Hoolan <rhoolan@gmail.com>
2023-01-17 12:47:19 +0900

    add initialize function

--- game.rb
+++ game.rb
@@ -1,3 +1,12 @@
+require_relative 'board'
+require_relative 'human_player'
+
 class Game 
 
+    def initialize(player1_mark, player2_mark)
+        @player1 = player1_mark
+        @player2 = player1_mark
+        @current_player = @player1
+
+    end
 end

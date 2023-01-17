a9de5ecbe7eb2901d25c8670826bdcebc05c5c18
Rob Hoolan <rhoolan@gmail.com>
2023-01-17 13:59:57 +0900

    add play function

--- game.rb
+++ game.rb
@@ -2,9 +2,10 @@ require_relative 'board'
 require_relative 'human_player'
 
 class Game 
-    attr_reader :current_player, :player1, :player2
+    attr_reader :board, :current_player, :player1, :player2
 
     def initialize(player1_mark, player2_mark)
+        @board = Board.new
         @player1 = player1_mark
         @player2 = player2_mark
         @current_player = player1
@@ -17,4 +18,20 @@ class Game
             @current_player = player1
         end
     end
+
+    def play
+        while board.empty_positions?
+            board.print_board
+            pos = current_player.get_position
+            board.place_mark(pos, current_player.mark)
+            if board.win?(current_player.mark)
+                puts 'You won!'
+                return
+            else
+                switch_turn
+            end
+            puts 'draw'
+        end
+    end
+    
 end

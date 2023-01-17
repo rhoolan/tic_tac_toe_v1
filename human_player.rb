e36862aeca8912b252d5c33ba0224b322898df91
Rob Hoolan <rhoolan@gmail.com>
2023-01-17 12:31:17 +0900

    add get_position function

--- human_player.rb
+++ human_player.rb
@@ -1,7 +1,25 @@
+require_relative 'board'
+
 class HumanPlayer
     attr_reader :mark
-    
+
     def initialize(mark_value)
         @mark = mark_value
     end
+
+
+    def get_position
+        position = [-1] #set to -1 because .between? wont work with nil class
+        board_length = 3
+
+        #ask for user input until they enter valid input
+        until position[0].to_i.between?(0,board_length-1) && position[-1].to_i.between?(0, board_length-1)
+            puts " #{mark}'s turn"
+            puts "Please enter a valid position (for example '1 1'): "
+            position = gets.chomp.split('')
+        end
+
+        return position
+    end
+
 end
